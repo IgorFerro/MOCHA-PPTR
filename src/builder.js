49937a8787fb4da8b3232bda0b9e19d24ef8d5e0
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-20 07:35:15 -0300

    Builder class part 2

--- src/builder.js
+++ src/builder.js
@@ -16,5 +16,21 @@ export default class Builder {
     const page = await browser.newPage();
     const extendPage = new Builder(page);
     await page.setDefaultNavigationTimeout(10000);
+
+    switch(viewport){
+     case "Mobile":
+         const mobileViewport = puppeteer.devices["iPhone X"];
+         await page.emulate(mobileViewport);
+         break;
+     case "Tablet":
+         const tabletViewport = puppeteer.devices["ipad landscape"];
+         await page.emulate(tabletViewport);
+         break;
+     case "Desktop":
+         await page.setViewport({width: 800, height: 600});
+         break;
+     default:
+         throw new Error("Supported devices are ony Mobile | Tablet | Desktop");
+    }
     }
 }
