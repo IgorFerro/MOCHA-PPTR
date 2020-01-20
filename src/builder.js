b7c259ebbd17ad32646ca6cf861dabbadd2e3806
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-20 07:48:08 -0300

    Builder Class part 03

--- src/builder.js
+++ src/builder.js
@@ -1,15 +1,15 @@
 import puppeteer from "puppeteer";
 
 export default class Builder {
-    static async build(viewport){
+  static async build(viewport) {
     const launchOptions = {
-        headless: true,
-        slowMo: 0,
-        args:[
-            "--no-sandbox",
-            "--disable=setui-sandbox",
-            "--disable-web-security"
-        ]
+      headless: true,
+      slowMo: 0,
+      args: [
+        "--no-sandbox",
+        "--disable=setui-sandbox",
+        "--disable-web-security"
+      ]
     };
 
     const browser = await puppeteer.launch(launchOptions);
@@ -17,20 +17,28 @@ export default class Builder {
     const extendPage = new Builder(page);
     await page.setDefaultNavigationTimeout(10000);
 
-    switch(viewport){
-     case "Mobile":
-         const mobileViewport = puppeteer.devices["iPhone X"];
-         await page.emulate(mobileViewport);
-         break;
-     case "Tablet":
-         const tabletViewport = puppeteer.devices["ipad landscape"];
-         await page.emulate(tabletViewport);
-         break;
-     case "Desktop":
-         await page.setViewport({width: 800, height: 600});
-         break;
-     default:
-         throw new Error("Supported devices are ony Mobile | Tablet | Desktop");
+    switch (viewport) {
+      case "Mobile":
+        const mobileViewport = puppeteer.devices["iPhone X"];
+        await page.emulate(mobileViewport);
+        break;
+      case "Tablet":
+        const tabletViewport = puppeteer.devices["ipad landscape"];
+        await page.emulate(tabletViewport);
+        break;
+      case "Desktop":
+        await page.setViewport({ width: 800, height: 600 });
+        break;
+      default:
+        throw new Error("Supported devices are ony Mobile | Tablet | Desktop");
     }
+      return new Proxy(extendPage, {
+          get: function(_target, property){
+              return extendPage[property] || browser[property] || page[property];
+          }
+      });  
+  }
+    constructor(page){
+        this.page=page;
     }
 }
