845fe0e0f827b7492a69caefa694ca889edd80b2
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-21 12:19:10 -0300

    Applying Page Object

--- build/tests/example.test.js
+++ build/tests/example.test.js
@@ -10,13 +10,19 @@ var _builder2 = _interopRequireDefault(_builder);
 
 var _mocha = require("mocha");
 
+var _LoginPage = require("../pages/LoginPage");
+
+var _LoginPage2 = _interopRequireDefault(_LoginPage);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 (0, _mocha.describe)("Mocha steps demo", function () {
     var page = void 0;
+    var loginPage = void 0;
 
     before(async function () {
         page = await _builder2.default.build("Desktop");
+        loginPage = await new _LoginPage2.default(page);
     });
     after(async function () {
         await page.close();
@@ -24,19 +30,20 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
     (0, _mochaSteps.step)("should load zero bank homepage", async function () {
         await page.goto("http://zero.webappsecurity.com");
-        (0, _chai.expect)((await page.isElementVisible('#signin_button'))).to.be.true;
+        (0, _chai.expect)((await page.isElementVisible("#signin_button"))).to.be.true;
     });
 
     (0, _mochaSteps.step)("display login form", async function () {
-        await page.waitAndClick("signin_button");
-        (0, _chai.expect)((await page.isElementVisible("login_form"))).to.be.true;
+        await page.waitAndClick("#signin_button");
+        (0, _chai.expect)((await page.isElementVisible("#login_form"))).to.be.true;
         (0, _chai.expect)((await page.isElementVisible("#signin_button"))).to.be.false;
     });
 
     (0, _mochaSteps.step)("should login to appliction", async function () {
-        await page.waitAndType("#user_login", "username");
-        await page.waitAndType("#user_password", "password");
-        await page.waitAndClick(".btn-primary");
+        await loginPage.login("username", "password");
+        //await page.waitAndType("#user_login","username");
+        //await page.waitAndType("#user_password","password");
+        //await page.waitAndClick(".btn-primary");
         (0, _chai.expect)((await page.isElementVisible(".nav-tabs"))).to.be.true;
     });
     (0, _mochaSteps.step)("should have 6 navbar links", async function () {
--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -3,12 +3,15 @@ import {expect} from "chai";
 
 import Page from "../builder";
 import { describe } from "mocha";
+import LoginPage from '../pages/LoginPage'
 
 describe("Mocha steps demo", ()=>{
     let page;
+    let loginPage;
 
     before(async()=>{
         page= await Page.build("Desktop");
+        loginPage= await new LoginPage(page);
     });
     after(async ()=>{
         await page.close();
@@ -16,19 +19,20 @@ describe("Mocha steps demo", ()=>{
 
     step("should load zero bank homepage", async () => {
        await page.goto("http://zero.webappsecurity.com");
-       expect(await page.isElementVisible('#signin_button')).to.be.true;
+       expect(await page.isElementVisible("#signin_button")).to.be.true;
        })
 
     step("display login form", async()=> {
-        await page.waitAndClick("signin_button");
-        expect(await page.isElementVisible("login_form")).to.be.true
+        await page.waitAndClick("#signin_button");
+        expect(await page.isElementVisible("#login_form")).to.be.true
         expect(await page.isElementVisible("#signin_button")).to.be.false;
     })
 
     step("should login to appliction" , async ()=>{
-        await page.waitAndType("#user_login","username");
-        await page.waitAndType("#user_password","password");
-        await page.waitAndClick(".btn-primary");
+        await loginPage.login("username","password");
+        //await page.waitAndType("#user_login","username");
+        //await page.waitAndType("#user_password","password");
+        //await page.waitAndClick(".btn-primary");
         expect(await page.isElementVisible(".nav-tabs")).to.be.true;
     })
     step("should have 6 navbar links",async ()=>{
